f2972ad6597ef697db1e731e37374c06a3ac8a1b
enamorada <109630476+AlmaHakba@users.noreply.github.com>
2023-08-05 09:39:43 -0700

    Setup

---
+++ electron.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true, // Required for Next.js
+    },
+  });
+
+  const url = isDev
+    ? 'http://localhost:3000' // Development server URL
+    : `file://${path.join(__dirname, '../out/index.html')}`; // Build output directory
+
+  mainWindow.loadURL(url);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
