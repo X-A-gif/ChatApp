50e547b0f585dbbfbea43a7ce323f0102eb87523
enamorada <109630476+AlmaHakba@users.noreply.github.com>
2023-08-12 17:46:41 -0700

    GunDB connection

--- electron.js
+++ electron.js
@@ -1,59 +1,36 @@
 const { app, BrowserWindow } = require('electron');
-const isDev = require('electron-is-dev');
 const path = require('path');
-const http = require('http');
-const { Server } = require('socket.io');
 
 let mainWindow;
-const server = http.createServer();
-const io = new Server(server);
-
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  socket.on('message', (message) => {
-    io.emit('message', message);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
-});
-
-server.listen(3001, () => {
-  console.log('Socket.io server listening on :3001');
-});
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js'),
     },
   });
 
-  const url = isDev
-    ? 'http://localhost:3000'
-    : `file://${path.join(__dirname, '../out/index.html')}`;
-
-  mainWindow.loadURL(url);
+  mainWindow.loadURL('http://localhost:3000');
 
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
 }
 
-app.on('ready', createWindow);
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
-  }
-});
-
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow();
   }
 });
